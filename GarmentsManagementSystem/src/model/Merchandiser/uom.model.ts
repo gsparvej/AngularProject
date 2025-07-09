983bf042caf780130036e31354e79dcbe70b3c97
gsparvej <parvejmd4469@gmail.com>
2025-07-09 12:30:50 +0600

    added

--- GarmentsManagementSystem/src/model/Merchandiser/uom.model.ts
+++ GarmentsManagementSystem/src/model/Merchandiser/uom.model.ts
@@ -4,4 +4,9 @@ export class Uom{
     id !: string;
     productName!: string;
     size!: string;
+    body! : number;
+    sleeve! : number;
+    pocket!: number;
+    wastage!: number;
+    shrinkage!: number;
 }
